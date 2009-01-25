8017c7652fdf3767fac48cfa975041f54e979295
Aemon Cannon <aemoncannon@gmail.com>
2009-01-25 18:18:02 -0500

    Add reference to intrinsics in project

--- .as3-mode-project.el
+++ .as3-mode-project.el
@@ -1,1 +1,6 @@
-(setq as3-project-source-paths (list (concat as3-project-helper-project-root-dir "src/as3")))
+
+
+(setq as3-project-source-paths (list 
+				(concat as3-project-helper-project-root-dir "src/as3")
+				(expand-file-name "~/lib/as3_intrinsics")
+				))
